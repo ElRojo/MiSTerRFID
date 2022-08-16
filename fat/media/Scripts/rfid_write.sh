1600de5ac53317332e5b439dcf76fe8e3f959e3f
ElRojo <connerallred@icloud.com>
2022-08-16 17:46:50 -0600

    adding full filepath for sed command.

--- fat/media/Scripts/rfid_write.sh
+++ fat/media/Scripts/rfid_write.sh
@@ -11,10 +11,11 @@ write_rom()
     }
     gameTitle=$(get_title "$runningGame")
 
-  rfidFile=rfid_process.sh
+  rfidFile=/media/fat/Scripts/rfid_process.sh
   sed -i "/$cardNumber/d" "$rfidFile"
   # '13i...' is the starting line for the case-statement. Change this if you add code or newlines to rfid_process.sh
   sed -i "13i \"$cardNumber\") "play" \"$gameTitle\" ;;" "$rfidFile"
+  
 }
 
 write_rom "$1"
