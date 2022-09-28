2a73142df81274ae67db8befc990301d7f1db775
ElRojo <connerallred@icloud.com>
2022-09-27 22:26:35 -0600

    neo geo needs work.

--- fat/media/Scripts/rfid_write.sh
+++ fat/media/Scripts/rfid_write.sh
@@ -2,60 +2,63 @@
 
 write_rom() {
 
-	#==========================================
-	#             Env Variables              #
-	#==========================================
-	cardNumber="$1"
-	misterCmd=/dev/MiSTer_cmd
-	confFile=/media/fat/Scripts/game_list_rfid.conf
-	coreName=$(cat /tmp/CORENAME)
-	startPath=$(cat /tmp/STARTPATH)
-	fullPath=$(cat /tmp/FULLPATH)
-	game=$(cat /tmp/CURRENTPATH)
-	rootPath=/media/fat/
-	fullGameDir="$rootPath""$fullPath"/"$game"              #/media/fat/games/CORE/Gamedir
-	rbfFile=_$(cat /tmp/STARTPATH | awk -F _ '{printf $2}') #_Folder/CORE
+  #==========================================
+  #             Env Variables              #
+  #==========================================
+  cardNumber="$1"
+  misterCmd=/dev/MiSTer_cmd
+  confFile=/media/fat/Scripts/game_list_rfid.conf
+  coreName=$(cat /tmp/CORENAME)
+  startPath=$(cat /tmp/STARTPATH)
+  fullPath=$(cat /tmp/FULLPATH)
+  game=$(cat /tmp/CURRENTPATH)
+  rootPath=/media/fat/
+  fullGameDir="$rootPath""$fullPath"/"$game"              #/media/fat/games/CORE/Gamedir
+  rbfFile=_$(cat /tmp/STARTPATH | awk -F _ '{printf $2}') #_Folder/CORE
 
-	writeMgl() {
-		if [ ! -f "$SedPath" ]; then
-			echo "<mistergamedescription><rbf>"$rbfFile"</rbf><file delay=\"2\" type=\"f\" index=\"0\" path=\"../../"$relativeGameDir"\"/></mistergamedescription>" >"$sedPath"
-		fi
-	}
+  writeMgl() {
+    if [ ! -f "$SedPath" ]; then
+      echo "<mistergamedescription><rbf>"$rbfFile"</rbf><file delay=\"2\" type=\"f\" index=\"0\" path=\"../../"$relativeGameDir"\"/></mistergamedescription>" >"$sedPath"
+    fi
+  }
 
-	writeArcade() {
-		arcadeWithGame=$(cat /tmp/STARTPATH | awk -F /media/fat/ '{printf $2}' | awk -F .mra '{printf $1}')
-	}
+  writeArcade() {
+    arcadeWithGame=$(cat /tmp/STARTPATH | awk -F /media/fat/ '{printf $2}' | awk -F .mra '{printf $1}')
+  }
 
-	if [[ ${startPath} = *".mgl" ]]; then
-		return
-	fi
+  if [[ ${startPath} = *".mgl" ]]; then
+    return
+  fi
 
-	if [[ ${fullPath} != *_Arcade* ]]; then
-		fileFinder=$(ls -1 "$fullGameDir"/)
-		case $fileFinder in
-		*".cue") extension=".cue" ;;
-		*".chd") extension=".chd" ;;
-		*".sfc") extension=".sfc" ;;
-		*".smc") extension=".smc" ;;
-		*".gen") extension=".gen" ;;
-		*".nes") extension=".nes" ;;
-		*".md") extension=".md" ;;
-		*".neo") extension=".neo"
-		esac
-		relativeGameDir="$fullPath"/"$game"/"$game""$extension" #games/CORE/Gamedir/Game.EXTENSION
-		sedPath="$fullGameDir"/"$game""$extension".mgl          #/media/fat/games/CORE/Gamedir/Game.EXTENSION.mgl
-	elif [[ ${fullPath} = *_Arcade* ]]; then
-		extension=".mra"
-		sedPath="$rootPath""$arcadeWithGame""$extension"
-	fi
+  if [[ ${fullPath} != *_Arcade* ]]; then
+    if [[ ${coreName} = NEOGEO ]]; then
+      extension=''
+      rbfFile=_/Console/NEOGEO
+    fi
+    fileFinder=$(ls -1 "$fullGameDir"/)
+    case $fileFinder in
+    *".cue") extension=".cue" ;;
+    *".chd") extension=".chd" ;;
+    *".sfc") extension=".sfc" ;;
+    *".smc") extension=".smc" ;;
+    *".gen") extension=".gen" ;;
+    *".nes") extension=".nes" ;;
+    *".md") extension=".md" ;;
+    esac
+    relativeGameDir="$fullPath"/"$game"/"$game""$extension" #games/CORE/Gamedir/Game.EXTENSION
+    sedPath="$fullGameDir"/"$game""$extension".mgl          #/media/fat/games/CORE/Gamedir/Game.EXTENSION.mgl
+  elif [[ ${fullPath} = *_Arcade* ]]; then
+    extension=".mra"
+    sedPath="$rootPath""$arcadeWithGame""$extension"
+  fi
 
-	case $extension in
-	".mra") writeArcade ;;
-	*) writeMgl ;;
-	esac
+  case $extension in
+  ".mra") writeArcade ;;
+  *) writeMgl ;;
+  esac
 
-	sed -i "/$cardNumber/d" "$confFile"
-	sed -i "4i $cardNumber	echo load_core \"$sedPath\" > $misterCmd" "$confFile"
+  sed -i "/$cardNumber/d" "$confFile"
+  sed -i "4i $cardNumber	echo load_core \"$sedPath\" > $misterCmd" "$confFile"
 }
 
 write_rom "$1"
