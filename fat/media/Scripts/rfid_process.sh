54732765971232af79caa4502f9634f98a7b5c42
ElRojo <6269516+ElRojo@users.noreply.github.com>
2022-10-04 10:57:43 -0600

    Testing this.

--- fat/media/Scripts/rfid_process.sh
+++ fat/media/Scripts/rfid_process.sh
@@ -5,14 +5,14 @@ unset gamefound
 while read -r line; do
 	rfid=$(echo "$line" | cut -f1)
 	game=$(echo "$line" | cut -f2)
-	if [ "x$rfid" == "x$1" ]; then
+	if [[ "$rfid" = "$1" ]]; then
 		gamefound="$game"
 		break
 	fi
 done </media/fat/Scripts/rfid_util/game_list.conf
 
 if [ "x$gamefound" == "x" ]; then
-	echo "RFID tag not found into config file..."
+	echo "RFID tag not found in config file..."
 	return
 fi
 
